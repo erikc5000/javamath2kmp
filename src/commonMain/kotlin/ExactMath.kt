82832fee16ab538255f69d682d90b7a11736c458
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2021-04-27 09:16:58 -0400

    Add missing plusExact/minusExact overloads (#20)

--- src/commonMain/kotlin/ExactMath.kt
+++ src/commonMain/kotlin/ExactMath.kt
@@ -6,5 +6,8 @@ package dev.erikchristensen.javamath2kmp
 import kotlin.jvm.JvmMultifileClass
 import kotlin.jvm.JvmName
 
+infix fun Int.plusExact(other: Long): Long = this.toLong() plusExact other
+infix fun Int.minusExact(other: Long): Long = this.toLong() minusExact other
+
 infix fun Long.plusExact(other: Int): Long = this plusExact other.toLong()
 infix fun Long.minusExact(other: Int): Long = this minusExact other.toLong()
