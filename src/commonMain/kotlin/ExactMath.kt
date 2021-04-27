2e446156feb2f25350eae53e3e45d206b7368e80
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2021-04-27 10:06:16 -0400

    Add missing KDocs (#22)

--- src/commonMain/kotlin/ExactMath.kt
+++ src/commonMain/kotlin/ExactMath.kt
@@ -6,8 +6,26 @@ package dev.erikchristensen.javamath2kmp
 import kotlin.jvm.JvmMultifileClass
 import kotlin.jvm.JvmName
 
+/**
+ * Returns the sum of this value and another value, throwing an exception if overflow occurs.
+ * @throws ArithmeticException if overflow occurs
+ */
 infix fun Int.plusExact(other: Long): Long = this.toLong() plusExact other
+
+/**
+ * Returns the sum of this value and another value, throwing an exception if overflow occurs.
+ * @throws ArithmeticException if overflow occurs
+ */
+infix fun Long.plusExact(other: Int): Long = this plusExact other.toLong()
+
+/**
+ * Returns the difference of this value and another value, throwing an exception if overflow occurs.
+ * @throws ArithmeticException if overflow occurs
+ */
 infix fun Int.minusExact(other: Long): Long = this.toLong() minusExact other
 
-infix fun Long.plusExact(other: Int): Long = this plusExact other.toLong()
+/**
+ * Returns the difference of this value and another value, throwing an exception if overflow occurs.
+ * @throws ArithmeticException if overflow occurs
+ */
 infix fun Long.minusExact(other: Int): Long = this minusExact other.toLong()
