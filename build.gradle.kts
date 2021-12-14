918c3fc01b31c712f66efd8d5c6733a3417699f8
Erik Christensen <erikchristensen@comcast.net>
2021-12-14 17:10:25 -0500

    Bump Kotlin to 1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
-    kotlin("multiplatform") version "1.6.0"
+    kotlin("multiplatform") version "1.6.10"
     id("org.jetbrains.dokka") version "1.5.31"
     id("org.jetbrains.kotlinx.kover") version "0.4.2"
     `maven-publish`
