b6cece88f9391350c91fbb35383a9ed6f0b77261
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2022-04-04 10:52:53 -0400

    Bump Kover to 0.5.0 (#50)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 plugins {
     kotlin("multiplatform") version "1.6.20"
     id("org.jetbrains.dokka") version "1.6.0"
-    id("org.jetbrains.kotlinx.kover") version "0.4.4"
+    id("org.jetbrains.kotlinx.kover") version "0.5.0"
     `maven-publish`
     signing
 }
