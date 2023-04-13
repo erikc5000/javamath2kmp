ba96e35cf316f9c58865f6f6d469ac66e23f0a67
Erik Christensen <40830816+erikc5000@users.noreply.github.com>
2023-04-13 10:56:53 -0400

    Update supported targets to match tier 1-3 (#83)

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,14 +24,12 @@ kotlin {
             nodejs()
             browser()
         },
-        iosArm32(),
         iosArm64(),
         iosX64(),
         iosSimulatorArm64(),
         macosX64(),
         macosArm64(),
         watchosArm64(),
-        watchosX86(),
         watchosX64(),
         watchosSimulatorArm64(),
         watchosDeviceArm64(),
@@ -39,10 +37,12 @@ kotlin {
         tvosX64(),
         tvosSimulatorArm64(),
         mingwX64(),
-        mingwX86(),
         linuxArm64(),
-        linuxArm32Hfp(),
-        linuxX64()
+        linuxX64(),
+        androidNativeArm32(),
+        androidNativeArm64(),
+        androidNativeX86(),
+        androidNativeX64()
     )
 
     sourceSets {
